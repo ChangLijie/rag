b43068783cdccc33718b8b85fcd7126e7342cd47
ChangLijie <lichieh_chang@innodisk.com>
2024-06-28 10:03:35 +0800

    feat : download model from hugging face.

--- tools/download_model.py
+++ tools/download_model.py
@@ -1,11 +1,13 @@
 from huggingface_hub import hf_hub_download, snapshot_download
+from setting import MODEL_LIST, SAVE_FOLDER
 
-REPO_ID = "HuggingFaceH4/zephyr-7b-beta"
-FILENAME = "all-MiniLM-L6-v2.joblib"
-save_folder = "./test/"
+# REPO_ID = "HuggingFaceH4/zephyr-7b-beta"
+# FILENAME = "all-MiniLM-L6-v2.joblib"
+
 
 # Mode1: Download one file.
 # model = hf_hub_download(repo_id=REPO_ID, filename=FILENAME,cache_dir=save_folder)
 
 # Mode2: Download all repository.
-snapshot_download(REPO_ID, cache_dir=save_folder)
+for model in MODEL_LIST:
+    snapshot_download(model, cache_dir=SAVE_FOLDER)
---
+++ tools/setting.py
@@ -0,0 +1,3 @@
+HUGGING_FACE_TOKEN = "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"
+SAVE_FOLDER = "./model"
+MODEL_LIST = ["sentence-transformers/all-MiniLM-L6-v2", "HuggingFaceH4/zephyr-7b-beta"]
